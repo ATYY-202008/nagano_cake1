0ffde6d7aef81e1198d9bd548e27dd5fe71898a6
vagrant <vagrant@localhost.localdomain>
2020-09-19 03:08:33 +0000

    追加

--- db/schema.rb
+++ db/schema.rb
@@ -26,14 +26,6 @@ ActiveRecord::Schema.define(version: 2020_09_16_114649) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.string "postal_code"
-    t.string "address"
-    t.string "telephone_number"
-    t.boolean "is_deleted"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -54,6 +46,14 @@ ActiveRecord::Schema.define(version: 2020_09_16_114649) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postal_code"
+    t.string "address"
+    t.string "telephone_number"
+    t.boolean "is_deleted"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
