2f1f79899df2044bee7573fdf9029618d7cd2b41
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-09-21 04:42:10 +0000

    [Add] order_details

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -1,5 +1,12 @@
 class Admins::OrderDetailsController < ApplicationController
 	def update
-		
+		@order_detail = OrderDetail.find_by(order_id: params[:order_id], item_id: params[:item_id])
+		@order_detail.update(order_detail_params)
+		redirect_to admins_order_path(@order_detail)
+	end
+
+	private
+	def order_detail_params
+	  params.require(:order_detail).permit(:making_status)
 	end
 end
