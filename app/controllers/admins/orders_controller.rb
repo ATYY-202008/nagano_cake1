9cdc6011de2bac75a38f136cd182ff86fcfb2977
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-09-21 09:55:43 +0000

    [Add]orders

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,8 +1,9 @@
 class Admins::OrdersController < ApplicationController
 	def index
-		
+		@orders = Order.all.page(params[:page]).per(10)
 	end
 	def show
-		
+		@order = Order.find(params[:id])
+		@order_deatils = OrderDetail.all
 	end
 end
