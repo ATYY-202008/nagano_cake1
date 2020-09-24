cd1e62d87c7dd848f965f68d73bf85d76b325804
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-09-24 06:42:33 +0000

    [Add]自動更新機能

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,7 +1,7 @@
 class Admins::CustomersController < ApplicationController
 
 	def index
-		@customers = Customer.all.page(params[:page]).per(10)
+		@customers = Customer.all.page(params[:page]).per(2)
 	end
 	def show
 		@customer = Customer.find(params[:id])
