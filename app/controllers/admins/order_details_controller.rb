8fcc8809d3e5a95c86909230ac688f62c7c23a72
AR-koki <vagrant@localhost.localdomain>
2020-09-24 12:25:24 +0000

    [Add]validation

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -1,11 +1,19 @@
 class Admins::OrderDetailsController < ApplicationController
 	def update
 		@order_detail = OrderDetail.find(params[:id])
+		@order = @order_detail.order
 		if @order_detail.update(order_detail_params)
 			redirect_to admins_order_path(@order_detail.order_id), notice: "You have updated making_status successfully."
 		else
 			render template: 'orders/show'
 		end
+
+		# 自動更新機能 
+		if @order_detail.making_status == "製作中" 
+			@order.update(status: "製作中") 
+		 end 
+		# 自動更新機能 
+
 	end
 
 	private
