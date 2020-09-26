1755ae9070c194740df479c80ea23c6dbc0a4f4d
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-09-26 14:36:07 +0000

    [Fix]customers_items

--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -7,7 +7,7 @@ class Customers::ItemsController < ApplicationController
 	        @items = @genre.items.page(params[:page]).per(8)
 	        @item = @items.where(params[:id]).count
 	    else
-	    	@items = Item.page(params[:page]).per(8)
+	    	@items = Item.joins(:genre).where(genres: {is_active: true}).page(params[:page]).per(8)
 	    	@item = Item.where(params[:id]).count
 	    end
 	end
