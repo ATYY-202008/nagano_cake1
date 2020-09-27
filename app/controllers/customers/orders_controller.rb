baf1416633beac0f947c775032d7b2d45e0a9524
daisuke33 <daisukeyao@outlook.jp>
2020-09-27 04:51:45 +0000

    [Fix]アクション内定義

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -2,64 +2,6 @@ class Customers::OrdersController < ApplicationController
     before_action :authenticate_customer!
     before_action :set_customer
 
-    def index
-        @orders = @customer.orders
-    end
-    
-    def create
-        if current_customer.cart_items.exists?
-            @order = Order.new(order_params)
-            @order.customer_id = current_customer.id
-            @add = params[:order][:add].to_i
-            case @add
-            when 1
-                @order.postal_code = @customer.postal_code
-                @order.address = @customer.address
-                @order.name = @customer.last_name + @customer.first_name
-            when 2
-                @order.postal_code = params[:order][:postal_code]
-                @order.address = params[:order][:address]
-                @order.name = params[:order][:name]
-            when 3
-                @order.postal_code = params[:order][:postal_code]
-                @order.address = params[:order][:address]
-                @order.name = params[:order][:name]
-            end
-            @order.save
-
-            if Address.find_by(address: @order.address).nil?
-            @address = Address.new
-            @address.postal_code = @order.postal_code
-            @address.address = @order.address
-            @address.name = @order.name
-            @address.customer_id = current_customer.id
-            @address.save
-            end
-
-        current_customer.cart_items.each do |cart_item|
-            order_details = @order.order_details.build
-            order_details.order_id = @order.id
-            order_details.item_id = cart_item.item_id
-            order_details.amount = cart_item.amount
-            order_details.price_tax = cart_item.item.price
-            order_details.save
-            cart_item.destroy 
-        end
-            render :thanks
-        else
-            redirect_to customers_customer_top_path
-            flash[:danger] = 'カートが空です。'
-        end
-    end
-
-    def show
-        @order = Order.find(params[:id])	
-        if @order.customer_id != current_customer.id
-            redirect_to root_path
-            flash[:alert] = "アクセスに失敗しました。"
-        end
-    end
-
     def new
         @order = Order.new
     end
@@ -76,7 +18,7 @@ class Customers::OrdersController < ApplicationController
                 @order.address = @customer.address
                 @order.name = @customer.last_name + @customer.first_name
             when 2
-                @sta = params[:order][:address].to_i
+                @sta = params[:order][:address_id].to_i
                 @address = Address.find(@sta)
                 @order.postal_code = @address.postal_code
                 @order.address = @address.address
@@ -87,8 +29,54 @@ class Customers::OrdersController < ApplicationController
                 @order.name = params[:order][:new_add][:name]
         end
     end
+    
+    def create
+        if current_customer.cart_items.exists?
+            @order = Order.new(order_params) 
+            @order.customer_id = current_customer.id
+            @order.status = "入金待ち"
+            @order.save
+            
+        if Address.find_by(address: @order.address).nil?
+            @address = Address.new
+            @address.postal_code = @order.postal_code
+            @address.address = @order.address
+            @address.name = @order.name
+            @address.customer_id = current_customer.id
+            @address.save
+        end
+
+        current_customer.cart_items.each do |cart_item|
+            order_detail = @order.order_details.build
+            order_detail.making_status = "着手不可"
+            order_detail.order_id = @order.id
+            order_detail.item_id = cart_item.item_id
+            order_detail.amount = cart_item.amount
+            order_detail.price_tax = cart_item.item.price
+            order_detail.save
+            cart_item.destroy 
+        end
+            redirect_to customers_order_thanks_path
+        else
+            redirect_to customers_customer_top_path
+            flash[:danger] = 'カートが空です。'
+        end
+    end
+
 
     def thanks
+    end
+
+    def index
+        @orders = Order.where(customer_id:[current_customer.id])
+    end
+
+    def show
+        @order = Order.find(params[:id])	
+        if @order.customer_id != current_customer.id
+            redirect_to root_path
+            flash[:alert] = "アクセスに失敗しました。"
+        end
     end
 
     private
@@ -97,9 +85,6 @@ class Customers::OrdersController < ApplicationController
     end
 
     def order_params
-        params.require(:order).permit(
-        :custmoer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status, :created_at,
-        order_items_attributes: [:order_id, :item_id, :price_tax, :amount, :making_status]
-        )
+        params.require(:order).permit(:custmoer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
     end
 end
