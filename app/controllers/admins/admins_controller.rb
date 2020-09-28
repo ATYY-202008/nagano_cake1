b6e28abbb1be3844d1432e719b784e1946491e3d
AR-koki <vagrant@localhost.localdomain>
2020-09-28 07:10:45 +0000

    [Add]before_action

--- app/controllers/admins/admins_controller.rb
+++ app/controllers/admins/admins_controller.rb
@@ -1,4 +1,5 @@
 class Admins::AdminsController < ApplicationController
+	before_action :authenticate_admin!
 	def top
 		@orders = Order.all
 		@orders_count = @orders.where(created_at: (Time.now.midnight - 1.day)..Time.now.midnight).count
